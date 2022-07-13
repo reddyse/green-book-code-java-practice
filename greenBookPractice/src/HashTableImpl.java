dce7a0cd47a0de231fd892ea421ee8a0ab75c299
Praneeth Reddy <praneeth_reddy@outlook.com>
2022-07-13 11:18:20 -0700

    Added HashTableImpl to check if string is unique

--- greenBookPractice/src/HashTableImpl.java
+++ greenBookPractice/src/HashTableImpl.java
@@ -1,3 +1,46 @@
+import java.util.Arrays;
+
 public class HashTableImpl {
-    //Create Hash Table Implementation
+    /*Determine if string has all unique characters without using any additional data structure
+        1. Create an array with size 26
+        2. Create a hash function to calculate the position to insert
+        3. While inserting check if string is unique
+        4. Insert, if already inserted, return not unique else return unique*/
+
+    final String[] theArray;
+    final int arraySize;
+
+    //Entry point of the program
+    public static void main(String[] args){
+        System.out.println("Program Start");
+        HashTableImpl arr = new HashTableImpl(26);
+        arr.checkIfUniqueHashFunc("aaz",arr.theArray);
+        arr.displayTheStack();
+    }
+
+    //Hash function that determines the location of the insert and also checks if the character is already inserted
+    public void checkIfUniqueHashFunc(String input, String[] arr){
+        char[] charArr= input.toCharArray();
+        for (char ch : charArr){
+            int index = (int) ch -97;
+            if(arr[index].equals("-1"))
+                arr[index] = "0";
+            else
+                System.out.println("The String is not unique");
+        }
+
+    }
+
+    //Constructor to initialize the Hash Table Array
+    HashTableImpl(int size){
+        arraySize = size;
+        theArray = new String[size];
+        Arrays.fill(theArray, "-1");
+    }
+
+    //Function to display the array
+    public void displayTheStack(){
+        for(int i =0; i<arraySize;i++)
+            System.out.print(theArray[i]);
+    }
 }
